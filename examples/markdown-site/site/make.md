f8e6684297a3017977932ed730d28b97b9bd070f
Mads Hartmann Jensen <mads379@gmail.com>
2016-08-04 22:17:01 +0200

    Blog post on Make

---
+++ examples/markdown-site/site/make.md
@@ -0,0 +1,7 @@
+# make
+Make is a great tool. I like to quote myself
+
+> I like to think of Make as a declarative string-oriented functional
+> programming language with very good support for executing commands
+> in a sub-shell. It's a programming language that happens to be very
+> good at creating build systems.
--- init.el
+++ init.el
@@ -8,49 +8,48 @@
 (require 'ob-python)
 (require 'ob-js)
 (require 'ob-R)
+(require 'ob-makefile)
+
+(setq auto-save-default nil) ; disable auto-save files (#foo#)
+(setq backup-inhibited t)    ; disable backup files (foo~)
 
 (setq org-publish-use-timestamps-flag nil)
 (setq org-html-htmlize-output-type 'css)
-(setq org-src-fontify-natively t)   ;trying it out
-(setq org-startup-folded nil)
 (setq org-confirm-babel-evaluate nil) ;; Living on the edge
-(setq org-startup-indented nil)
 (setq org-export-babel-evaluate nil) ;; Don't evaluate on export by default.
+(setq max-specpdl-size 10000)
+(setq debug-on-error t)
 
-(setq org-publish-project-alist
-      '(
-        ;;
-        ;; Blog
-        ;;
-        ("org-mads379.github.com"
-         ;; Path to your org files.
-         :base-directory "~/dev/mads379.github.com/_org/"
-         :base-extension "org"
-         ;; Path to your Jekyll project.
-         :publishing-directory "~/dev/mads379.github.com/"
-         :recursive t
-         :publishing-function org-html-publish-to-html
-         :headline-levels 4
-         :html-extension "html"
-         :body-only t) ;; Only export section between <body> </body>
+;; This is important, otherwise I can't tangle source blocks
+;; written in makefile mode.
+(setq org-src-preserve-indentation t)
 
-        ("org-static-mads379.github.com"
-         :base-directory "~/dev/mads379.github.com/org/"
-         :base-extension "css\\|js\\|png\\|jpg\\|gif"
-         :publishing-directory "~/dev/mads379.github.com/"
-         :recursive t
-         :publishing-function org-publish-attachment)
-
-        ("mads379.github.com"
-         :components ("org-ianbarton" "org-static-ian"))))
+;; Even if sh-mode source-blocks fail I still want the output.
+(setq org-babel-default-header-args:sh
+      '((:prologue . "exec 2>&1") (:epilogue . ":")))
 
 (setq org-babel-load-languages
       '((ocaml . t)
         (emacs-lisp . t)
         (sh . t)
+        (makefile . t)
         (sql . t)
         (python . t)
         (js . t)
         (r . R)))
+
+(setq org-publish-project-alist
+      '(
+        ("org-mads379.github.com"
+         ;; Path to your org files.
+         :base-directory "~/dev/mads379.github.com/_org/"
+         :base-extension "org"
+         :publishing-directory "~/dev/mads379.github.com"
+         :publishing-function org-html-publish-to-html
+
+         :recursive t
+         :headline-levels 4
+         :html-extension "html"
+         :body-only t))) ;; Only export section between <body> </body>
 
 ;;; init.el ends here
